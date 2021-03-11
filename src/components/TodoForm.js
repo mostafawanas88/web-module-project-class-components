9974dee528d929df1aa82bcff4ebd3e90b6c46d7
mostafawanas88 <mostafawanas@hotmail.com>
2021-03-11 11:33:57 +0400

    draft

---
+++ src/components/TodoForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            task: ''
+        }
+    }
+    render() {
+        <form>
+
+        </form>
+
+    }
+}
+
+export default TodoForm;
